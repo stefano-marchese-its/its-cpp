3d58b2c8424ee0581474c1bda6bea9d51eead06e
stefano-marchese-its <stefano.marchese.its@gmail.com>
2024-08-20 19:24:49 +0200

    Create HelloWorld_01.cpp

---
+++ 01/HelloWorld_01.cpp
@@ -0,0 +1,13 @@
+// Learning C++ 
+// Exercise 00
+// Using the exercise files on GitHub, by Stefano
+
+#include <iostream>
+
+int main(){
+    
+  std::cout << "Hello World 01!!!\n";
+  std::cout << std::endl << std::endl;
+  
+    return (0);
+}
