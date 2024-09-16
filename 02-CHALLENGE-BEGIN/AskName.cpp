9b1d4554c21a43b568737d7e3e13c222cf0689c6
stefano-marchese-its <stefano.marchese.its@gmail.com>
2024-09-16 16:11:38 +0200

    Created AskName.cpp

---
+++ 02-CHALLENGE-BEGIN/AskName.cpp
@@ -0,0 +1,17 @@
+// Learning C++
+// ask the name of the user
+// and print a message with the name just inserted
+
+#include <iostream>
+#include <string>
+
+int main () {
+    std::string name;
+    std::cin >> name;
+
+    std::cout << name;
+
+    std::cout << std::endl << std::endl;
+
+    return(0);
+}
