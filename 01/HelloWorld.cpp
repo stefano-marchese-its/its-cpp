fee7670ab5e2f3db532b8b0d7491604e0ce0ca02
Wazard <34705162+Wazard@users.noreply.github.com>
2024-09-16 14:31:08 +0000

    si

--- 01/HelloWorld.cpp
+++ 01/HelloWorld.cpp
@@ -1,16 +1,12 @@
-// this is a comment: single line
+// Learning C++ 
+// Exercise 00
+// Using the exercise files on GitHub, by Stefano
 
-/* this is a comment
-   multi lines */
-
-// preprocessor directive
-// library IO Stream, C++ stardard library, which contains objects and functions to print text to the screen
 #include <iostream>
 
-// main entry poit to the program
-// the function tha is executed by default
-int main() {
-  std::cout << "Hi there!!! This is Stefano!!!" << std::endl;
-  std::cout << std::endl << std::endl;
-  return(0);
+using namespace std;
+
+int main(){
+    cout << "Stessa roba ma diversa ig!!!\n";
+    return 0;
 }
