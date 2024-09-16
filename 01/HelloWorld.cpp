a54b65d71cb67794d24b81e5b0a82470d25e9d58
stefano-marchese-its <stefano.marchese.its@gmail.com>
2024-09-16 15:11:55 +0200

    Created HelloWorld.cpp

---
+++ 01/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// this is a comment: single line
+
+/* this is a comment
+   multi lines */
+
+// preprocessor directive
+// library IO Stream, C++ stardard library, which contains objects and functions to print text to the screen
+#include <iostream>
+
+// main entry poit to the program
+// the function tha is executed by default
+int main() {
+  std::cout << "Hi there!!! This is Stefano!!!" << std::endl;
+  std::cout << std::endl << std::endl;
+  return(0);
+}
