45dcc95c9c12774929118c0d24af9c78a4a647bf
stefano-marchese-its <stefano.marchese.its@gmail.com>
2024-10-30 15:22:40 +0000

    created Deques.cpp

---
+++ 220/Deques.cpp
@@ -0,0 +1,59 @@
+// Learning C++
+// Iterators examples
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+#define MY_ENDL cout << endl << endl
+
+int main() {
+
+    deque<int> myDeque;
+
+    for (int i = 0; i < 11; ++i) {
+        myDeque.push_back(i);
+    }
+
+    for (auto& d : myDeque) {
+        cout << d << " ";
+    }
+    MY_ENDL;
+
+    for (int i = 0; i < 11; ++i) {
+        myDeque.push_front(i);
+    }
+
+    for (auto& d : myDeque) {
+        cout << d << " ";
+    }
+    MY_ENDL;
+
+    myDeque.pop_back();
+    myDeque.pop_front();
+
+    for (auto& d : myDeque) {
+        cout << d << " ";
+    }
+    MY_ENDL;
+
+    myDeque.push_back(99);
+    myDeque.push_front(9999);
+
+    for (auto& d : myDeque) {
+        cout << d << " ";
+    }
+    MY_ENDL;
+
+    reverse(myDeque.begin(), myDeque.end());
+
+    for (auto& d : myDeque) {
+        cout << d << " ";
+    }
+    MY_ENDL;
+
+    MY_ENDL;
+    
+}
