81f052556857818270d5fbe5d4c43b490d7d7945
stefano-marchese-its <stefano.marchese.its@gmail.com>
2024-11-04 14:06:51 +0000

    created MultiMaps.cpp

---
+++ 260/MultiMaps.cpp
@@ -0,0 +1,42 @@
+// Learning C++
+// MultiMaps examples
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+inline void my_endl() {std::cout << endl << endl;}
+
+inline void print_grades(multimap<string, int>& grades) {
+    for (auto& gr : grades) {
+        std::cout << gr.first << "-" << gr.second << " ";
+    }
+    my_endl();
+}
+
+int main() {
+
+    multimap<string, int> grades;
+
+    grades.insert({"Pippo", 9});
+    grades.insert({"Paperino", 10});
+    grades.insert({"Topolino", 8});
+    grades.insert({"Paperoga", 7});
+
+    grades.insert(pair<string,int>("Paperone", 10));
+    grades.insert(pair<string,int>("Paperone", 7));
+    grades.insert(pair<string,int>("Paperone", 6));
+
+    print_grades(grades);
+
+    auto range = grades.equal_range("Paperone");
+    cout << "Grades Paperone:" << endl;
+    for (auto it = range.first; it != range.second; ++it) {
+        cout << it->second << " ";
+    }
+    my_endl();
+
+    return 0;    
+}
